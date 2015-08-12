caf29adae2cf0c003ee9857338a2054429962c49
Timothy Sesler <tds45@case.edu>
2015-08-12 16:42:37 -0400

    Initial commit.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform CamPOS;
+	public float FollowRate;
+
+	// Use this for initialization
+	void Start () {
+		transform.rotation = CamPOS.rotation;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, CamPOS.position, Time.deltaTime * FollowRate);
+		transform.rotation = Quaternion.Slerp(transform.rotation, CamPOS.rotation, Time.deltaTime);
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float Velocity;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.W)) {
+			transform.Translate(Vector3.forward * Time.deltaTime * Velocity);
+		}
+
+
+	}
+}
